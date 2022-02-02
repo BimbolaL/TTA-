9bdf0b0949fed35729093481c3ef77dedf9d126d
Bimbola Lasisi <78049244+blasisi@users.noreply.github.com>
2022-02-02 18:34:05 +0000

    task1 flow diagram

--- Week 2/task1.py
+++ Week 2/task1.py
@@ -1,2 +1,2 @@
 # TASK1
-# Write a progra that ask the user for an integer number and check if its > 10. if its is, it will print "Nuber is Greater than 10", else "Nuber is smaller"
+# Write a program that ask the user for an integer number and check if its > 10. if its is, it will print "Nuber is Greater than 10", else "Nuber is smaller"
