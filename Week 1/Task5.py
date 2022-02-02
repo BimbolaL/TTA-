4861dd71988c5ba9bd132a1891943b62586dc05c
Bimbola Lasisi <78049244+blasisi@users.noreply.github.com>
2022-02-02 14:58:19 +0000

    task 5 completed

---
+++ Week 1/Task5.py
@@ -0,0 +1,21 @@
+# Task 5
+# Write a program which will ask for two numbers from a user.
+num1 = input("What is your first number?")
+num2 = input("What is your second number?")
+
+print(num1)
+print(num2)
+
+#  Then offer a menu to the user giving them a choice of operator:e.g. –Enter “a” if you want to add“b” if you want to subtractInclude +, -, /, *, ** square (to the power of). Once the user has selected which operator they wish to use, perform the calculation.
+
+
+def my_calculation():
+    print(" the sum of two  is equal to " + add)
+
+
+add = input("number1 + number2")
+sub = input("number1 - number2")
+div = input("number1 / number2")
+multiply = input("number1 * number2")
+
+my_calculation()
