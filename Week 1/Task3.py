a5e0724a1dae6c93fd5d7058fa8f0ad2d814d59f
Bimbola Lasisi <78049244+blasisi@users.noreply.github.com>
2022-02-02 14:44:25 +0000

    task 3 completed

---
+++ Week 1/Task3.py
@@ -0,0 +1,9 @@
+# Task3
+# Write a progra that allows user to enter their favorite starter, ain coure, dessert and drink.
+starter = "Jollof rice" 
+mainCourse = "Pounded Yam with Egusi soup " 
+dessert = "olives  and cheesecake" 
+drink = "Red Wine."
+# concatenate theses and output a mesaage which says ="Your favorite meal is ...with a glass of .."
+
+print("Your favourite meal is "  +  starter +  mainCourse  +  dessert + "  with a glass of " + drink)
